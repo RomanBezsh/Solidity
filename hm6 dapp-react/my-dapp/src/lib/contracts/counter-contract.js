f83d49eaac02cb6648ce8cd9d301e0b16138f906
RomanBezsh <153326902+RomanBezsh@users.noreply.github.com>
2026-02-19 12:56:30 +0200

    Delete hm6 dapp-react directory

--- hm6 dapp-react/my-dapp/src/lib/contracts/counter-contract.js
+++
@@ -1,7 +0,0 @@
-import web3 from "../web3";
-import counterAbi from "./abi/counterAbi.json";
-
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const counterContract = new web3.eth.Contract(counterAbi, contractAddress);
-
-export default counterContract;
--- hm6 dapp-react/my-dapp/src/lib/contracts/mediaStoreContract.js
+++
@@ -1,7 +0,0 @@
-import web3 from "../web3";
-import mediaStoreAbi from "./abi/mediaStoreAbi.json"
-
-const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
-const mediaStoreContract = new web3.eth.Contract(mediaStoreAbi, contractAddress);
-
-export default mediaStoreContract;
