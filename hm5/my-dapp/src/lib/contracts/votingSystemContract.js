362fc95816c381eb8eb91170f573e29107912fdd
RomanBezsh <153326902+RomanBezsh@users.noreply.github.com>
2026-02-19 16:44:49 +0200

    Fix import path for web3 in votingSystemContract

--- hm5/my-dapp/src/lib/contracts/votingSystemContract.js
+++ hm5/my-dapp/src/lib/contracts/votingSystemContract.js
@@ -1,7 +1,8 @@
-import web3 from "../../../lib/web3";
+import web3 from "../lib//web3";
 import votingSystem from "./abi/votingSystemAbi.json"
 
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const votingSystemContract = new web3.eth.Contract(votingSystem, contractAddress);
 
 export default votingSystemContract;
+
